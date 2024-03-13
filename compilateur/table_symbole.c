41d230279acb2ff3de996758d9ced5ab154e8912
Apollinsa <criquet@insa-toulouse.fr>
2024-03-13 09:53:24 +0100

    ajout des priorite

--- compilateur/table_symbole.c
+++ compilateur/table_symbole.c
@@ -1,18 +1,17 @@
 #include "table_symbole.h"
 #include <stdlib.h>
-
+#include <string.h>
 
 
 typedef struct {
     char* nom;
     int priority;
-    
 } symbole;
 
 
 
 
-symbole* Tab[1000]; 
+symbole* Tab[1024]; 
 int index = 0;
 int priority =0;
 
@@ -27,10 +26,14 @@ void set (char* nomvar) {
 
 int get (char* nomvar) {
 
-    int out=0;
+    int out=index-1;
 
-    while(Tab[out]!=nomvar){
-        out++;
+    while(!strncmp(Tab[out]->nom,nomvar) && out !=0){
+        out --;
+    }
+
+    if (!strncmp(Tab[out]->nom,nomvar)){
+        out = -1;
     }
 
     return out;
