4e9b3e7d1bb607648601249bd4bad2f3b0eb4c26
Jean-Yves <7360784+docjyJ@users.noreply.github.com>
2024-05-25 15:20:56 +0200

    Add pdf shemat
    Signed-off-by: Jean-Yves <7360784+docjyJ@users.noreply.github.com>

--- microproceseur/sources/DecodeStage.vhd
+++ microproceseur/sources/DecodeStage.vhd
@@ -3,12 +3,15 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE WORK.DRAGON.ALL;
 
 ENTITY DecodeStage IS PORT (
-    clk, rst             : IN std_logic;
-    instruction          : IN std_logic_vector(31 DOWNTO 0);
-    alu_op               : OUT std_logic_vector(3 DOWNTO 0);
-    alu_val_a, alu_val_b : OUT std_logic_vector(7 DOWNTO 0);
-    jump_to              : OUT std_logic_vector(7 DOWNTO 0)
-);
+    rst, wr  : IN std_logic;
+    code     : IN std_logic_vector(7 DOWNTO 0);
+    input_a  : IN std_logic_vector(7 DOWNTO 0);
+    input_b  : IN std_logic_vector(3 DOWNTO 0);
+    addr_wr  : IN std_logic_vector(3 DOWNTO 0);
+    val_wr   : IN std_logic_vector(7 DOWNTO 0);
+    output_a : OUT std_logic_vector(7 DOWNTO 0);
+    output_b : OUT std_logic_vector(7 DOWNTO 0);
+    jump     : OUT std_logic);
 END DecodeStage;
 
 ARCHITECTURE Behavioral OF DecodeStage IS
@@ -20,63 +23,28 @@ ARCHITECTURE Behavioral OF DecodeStage IS
         addr_wr        : IN std_logic_vector (3 DOWNTO 0);
         val_wr         : IN std_logic_vector (7 DOWNTO 0));
     END COMPONENT;
-    SIGNAL op_code, val_a, val_b : std_logic_vector(7 DOWNTO 0);
-    SIGNAL wr                    : std_logic;
-    SIGNAL addr_wr               : std_logic_vector(3 DOWNTO 0);
-    SIGNAL val_wr                : std_logic_vector(7 DOWNTO 0);
+    SIGNAL tmp_a : std_logic_vector(7 DOWNTO 0);
+    SIGNAL tmp_b : std_logic_vector(7 DOWNTO 0);
 BEGIN
     datas : Registers PORT MAP(
         rst     => rst,
-        addr_a  => instruction(11 DOWNTO 8),
-        addr_b  => instruction(3 DOWNTO 0),
-        val_a   => val_a,
-        val_b   => val_b,
+        addr_a  => input_a(3 DOWNTO 0),
+        addr_b  => input_b,
+        val_a   => tmp_a,
+        val_b   => tmp_b,
         wr      => wr,
-        addr_wr => addr_wr,
-        val_wr  => val_wr
+        addr_wr => code(7 DOWNTO 4),
+        val_wr  => input_b
     );
 
-    --missing PRINT LOAD STORE JUMPr loadr
-
-    op_code <= instruction(31 DOWNTO 28);
-
-    wr <= '1' WHEN op_code = op_copy OR op_code = op_define ELSE
+    jump <= '1' WHEN (tmp_b = "00000000" AND (code = op_branch OR code = op_branch_r)) OR code = op_jump OR code = op_jump_r ELSE
         '0';
-    addr_wr <= instruction(19 DOWNTO 16);
-    val_wr  <= instruction(15 DOWNTO 8) WHEN op_code = op_copy ELSE
-        val_a;
 
-    WITH op_code SELECT alu_op <=
-        s_add WHEN op_add,
-        s_mul WHEN op_multiply,
-        s_sub WHEN op_subtract,
-        s_div WHEN op_divide,
-        s_sub WHEN op_lower_than,
-        s_sub WHEN op_greater_than,
-        s_sub WHEN op_equal_to,
-        s_sub WHEN op_negate,
-        s_mod WHEN op_modulo,
-        log_and WHEN op_bitwise_and,
-        log_or WHEN op_bitwise_or,
-        log_eq WHEN op_bitwise_not,
-        log_xor WHEN op_bitwise_xor,
-        alu_nop WHEN OTHERS;
+    output_a <= input_a WHEN code = op_define OR code = op_jump OR code = op_branch ELSE
+        tmp_a;
 
-    WITH op_code SELECT alu_val_a <=
-        val_a WHEN op_add | op_multiply | op_subtract | op_divide | op_modulo |
-        op_lower_than | op_greater_than | op_equal_to |
-        op_bitwise_and | op_bitwise_or | op_bitwise_xor,
-        (OTHERS => '0') WHEN OTHERS;
+    output_b <= tmp_b;
 
-    WITH op_code SELECT alu_val_b <=
-        val_b WHEN op_add | op_multiply | op_subtract | op_divide | op_modulo |
-        op_lower_than | op_greater_than | op_equal_to |
-        op_bitwise_and | op_bitwise_or | op_bitwise_xor,
-        val_a WHEN op_negate | op_bitwise_not,
-        (OTHERS => '0') WHEN OTHERS;
-
-    jump_to <= instruction(27 DOWNTO 20)
-        WHEN op_code = op_jump OR (op_code = op_branch AND val_a = "0000") ELSE
-        (OTHERS => '0');
+    --TODO Aléa
 
 END Behavioral;
