ee8f1ab8b00ea72f37066a10340c986f56579cd8
Jean-Yves <7360784+docjyJ@users.noreply.github.com>
2024-05-27 19:36:18 +0200

    fix io
    Signed-off-by: Jean-Yves <7360784+docjyJ@users.noreply.github.com>

--- microproceseur/sources/IOManager.vhdl
+++ microproceseur/sources/IOManager.vhdl
@@ -2,26 +2,25 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 
 ENTITY IOManager IS PORT (
-    clk, rst, test : IN std_logic;
-    rd, wr         : IN std_logic_vector(15 DOWNTO 0);
-    led            : OUT std_logic_vector(15 DOWNTO 0);
-    an             : OUT std_logic_vector(3 DOWNTO 0);
-    seg            : OUT std_logic_vector(6 DOWNTO 0);
-    dp             : OUT std_logic);
+    clk, rst, test, mode : IN std_logic;
+    rd, wr               : IN std_logic_vector(15 DOWNTO 0);
+    led                  : OUT std_logic_vector(15 DOWNTO 0);
+    an                   : OUT std_logic_vector(3 DOWNTO 0);
+    seg                  : OUT std_logic_vector(6 DOWNTO 0);
+    dp                   : OUT std_logic);
 END ENTITY;
 
 ARCHITECTURE Behavioral OF IOManager IS
     COMPONENT Counter IS GENERIC
         (N : integer := 8);
         PORT (
-            clk, rst, mode : IN std_logic;
-            en, dir, lod   : IN std_logic;
-            a              : IN std_logic_vector (N - 1 DOWNTO 0);
-            s              : OUT std_logic_vector (N - 1 DOWNTO 0));
+            clk, rst     : IN std_logic;
+            en, dir, lod : IN std_logic;
+            a            : IN std_logic_vector (N - 1 DOWNTO 0);
+            s            : OUT std_logic_vector (N - 1 DOWNTO 0));
     END COMPONENT;
 
     SIGNAL state : std_logic_vector(1 DOWNTO 0);
-    SIGNAL mode  : std_logic;
     SIGNAL digit : std_logic_vector(3 DOWNTO 0);
     SIGNAL clkms : std_logic;
     SIGNAL nul   : std_logic_vector(16 DOWNTO 0);
@@ -53,31 +52,30 @@ BEGIN
         END IF;
     END PROCESS;
 
-    mode <= '1';
-
     led <= x"FFFF" WHEN test = '1' ELSE
         wr;
 
     WITH test & mode & state SELECT digit <=
-    wr(3 DOWNTO 0) WHEN "0000",
-    wr(7 DOWNTO 4) WHEN "0001",
-    wr(11 DOWNTO 8) WHEN "0010",
-    wr(15 DOWNTO 12) WHEN "0011",
-    rd(3 DOWNTO 0) WHEN "0100",
-    rd(7 DOWNTO 4) WHEN "0101",
-    rd(11 DOWNTO 8) WHEN "0110",
-    rd(15 DOWNTO 12) WHEN "0111",
+    wr(15 DOWNTO 12) WHEN "0000",
+    wr(11 DOWNTO 8) WHEN "0001",
+    wr(7 DOWNTO 4) WHEN "0010",
+    wr(3 DOWNTO 0) WHEN "0011",
+    rd(15 DOWNTO 12) WHEN "0100",
+    rd(11 DOWNTO 8) WHEN "0101",
+    rd(7 DOWNTO 4) WHEN "0110",
+    rd(3 DOWNTO 0) WHEN "0111",
     "1000" WHEN OTHERS;
 
-    WITH state SELECT an <=
-        "0111" WHEN "00",
-        "1011" WHEN "01",
-        "1101" WHEN "10",
-        "1110" WHEN "11";
+    WITH rst & state SELECT an <=
+    "0111" WHEN "000",
+    "1011" WHEN "001",
+    "1101" WHEN "010",
+    "1110" WHEN "011",
+    "1111" WHEN OTHERS;
 
     WITH digit SELECT seg <=
         "1000000" WHEN "0000", -- 0
-        "0000110" WHEN "0001", -- 1
+        "1111001" WHEN "0001", -- 1
         "0100100" WHEN "0010", -- 2
         "0110000" WHEN "0011", -- 3
         "0011001" WHEN "0100", -- 4
